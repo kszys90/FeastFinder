16b47edde6132c05d49fd6fe7f587bc70c6040b6
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-03-20 20:04:16 +0100

    added react-icons, added menu icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { NavLink } from 'react-router-dom'
+import { GiHamburgerMenu } from "react-icons/gi";
+
 
 export default function Header() {
     return (
         <>
             <header id="main-header">
                 <h1 className='decorated-text header-title'>Feast Finder</h1>
-                <nav >
+                <nav>
+                    <GiHamburgerMenu className="header__menu-icon" />
+
                     <ul className="header__nav-list">
                         <li>
                             <NavLink to="/" className={({ isActive }) => isActive ? 'link--active' : ''} end>Home</NavLink>
